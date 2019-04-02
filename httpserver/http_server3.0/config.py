e177b51fe55a34e6dc36bc614072e3ce0d7f30a2
wjl <1971413146@qq.com>
2019-04-02 14:15:32 +0800

    httpserver3.0 over

--- httpserver/http_server3.0/config.py
+++ httpserver/http_server3.0/config.py
@@ -8,3 +8,8 @@ PORT = 7777
 
 # 是否为调试模式
 DEBUG = True
+
+# Web Frame 应用服务地址
+frame_ip = '127.0.0.1'
+frame_port = 8888
+frame_address = (frame_ip,frame_port)
--- httpserver/http_server3.0/http_server3.0.py
+++ httpserver/http_server3.0/http_server3.0.py
@@ -2,8 +2,27 @@ from socket import *
 import sys
 from threading import Thread
 from config import *
+import json
+from time import sleep
 
 ADDR = (HOST,PORT)
+
+# 将请求发送给frame,获取数据
+def connect_frame(**env):
+    s = socket()
+    while True:
+        try:
+            s.connect(frame_address)
+        except Exception as e:
+            print(e)
+            sleep(3)
+        else:
+            break
+    # 将请求发送给webframe
+    s.send(json.dumps(env).encode())
+    data = s.recv(4096*20).decode()
+    return data
+    
 
 # 封装HTTPServer类，实现基本功能
 class HTTPServer(object):
@@ -37,6 +56,39 @@ class HTTPServer(object):
             client = Thread(target = self.handle,args = (connfd,))
             client.setDaemon(True)
             client.start()
+    def handle(self,connfd):
+        request = connfd.recv(4096)
+        if not request:
+            connfd.close()
+            return
+        request_lines = request.splitlines()
+        # 得到请求行
+        request_line = request_lines[0].decode()
+        print('请求:',request_line)
+
+        # 组织请求内容
+        tmp = request_line.split(' ')
+        method = tmp[0]
+        path_info = tmp[1]
+        # 连接frame 获取数据返回
+        data = connect_frame(method = method,path_info = path_info)
+        self.response(connfd,data)
+    
+    # 将数据组织响应格式发送给浏览器
+    def response(self,connfd,data):
+        # data ==>'{'status':'200','content':'xxxx'}'
+        data = json.loads(data)
+        status = data['status'] #  响应码
+        content = data['content']  #　数据内容
+        if status == '200':
+            response_headlers = 'HTTP/1.1 200 OK\r\n'
+        elif status == '404':
+            response_headlers = 'HTTP/1.1 404 Not Found\r\n'
+        response_headlers += '\r\n'
+        response_body = content
+        response = response_headlers + response_body
+        connfd.send(response.encode())
+        connfd.close() 
 
 httpd = HTTPServer(ADDR)
-http.serve_forever() # 启动服务
+httpd.serve_forever() # 启动服务
